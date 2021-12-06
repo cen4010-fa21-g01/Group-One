44f1d9cfa31e4011a35f6c994db87eb4ba15a370
JGSilverio <89946272+JGSilverio@users.noreply.github.com>
2021-12-05 20:37:04 -0500

    adding path route

--- coding/Milestone 3 (php)/about.php
+++ coding/Milestone 3 (php)/about.php
@@ -1,3 +1,8 @@
+<?php 
+include("path.php");
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -15,20 +20,8 @@
 
 <body>
     <!-- Responsive navbar-->
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-        <div class="container">
-            <a class="navbar-brand" href="starter.php">Sixth-Degree</a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
-                    <li class="nav-item"><a class="nav-link" href="starter.php">Home</a></li>
-                    <li class="nav-item"><a class="nav-link" href="about.php">About</a></li>
-                    <li class="nav-item"><a class="nav-link" href="setting.php">Setting</a></li>
-                    <li class="nav-item"><a class="nav-link active" aria-current="page" href="logout.php">Log out</a></li>
-                </ul>
-            </div>
-        </div>
-    </nav>
+    <?php include(ROOT_PATH . "/includes/header.php"); ?>
+
     <!-- Page header with logo and tagline-->
     <header class="py-5 bg-light border-bottom mb-4">
         <div class="container">
@@ -40,11 +33,8 @@
     </header>
 
     <!-- Footer-->
-    <footer class="py-5 bg-dark">
-        <div class="container">
-            <p class="m-0 text-center text-white">Copyright &copy; Your Website 2021</p>
-        </div>
-    </footer>
+    <?php include(ROOT_PATH . "/includes/footer.php"); ?>
+
     <!-- Bootstrap core JS-->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
     <!-- Core theme JS-->
