f6cbeeb49c382beec71e5dbcd90ca2e11d36c883
JGSilverio <89946272+JGSilverio@users.noreply.github.com>
2021-12-05 20:33:09 -0500

    adding path route

--- coding/Milestone 3 (php)/setting.php
+++ coding/Milestone 3 (php)/setting.php
@@ -1,3 +1,8 @@
+<?php 
+include("path.php");
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -14,21 +19,9 @@
 </head>
 
 <body>
-    <!-- Responsive navbar-->
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-        <div class="container">
-            <a class="navbar-brand" href="starter.php">Sixth-Degree</a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
-                    <li class="nav-item"><a class="nav-link" href="starter.php">Home</a></li>
-                    <li class="nav-item"><a class="nav-link" href="about.php">About</a></li>
-                    <li class="nav-item"><a class="nav-link" href="setting.php">Setting</a></li>
-                    <li class="nav-item"><a class="nav-link active" aria-current="page" href="logout.php">Log out</a></li>
-                </ul>
-            </div>
-        </div>
-    </nav>
+
+    <!--Navbar-->
+    <?php include(ROOT_PATH . "/includes/header.php"); ?>
 
     <!-- Page header with logo and tagline-->
     <header class="py-5 bg-light border-bottom mb-4">
@@ -36,17 +29,14 @@
             <div class="text-center my-5">
                 <h1 class="fw-bolder">User Setting:</h1>
                 <!-- Change Password -->
-                <a href="changep.php">Change Password</a>
+                <a href="changep.html">Change Password</a>
             </div>
         </div>
     </header>
 
-    <!-- Footer-->
-    <footer class="py-5 bg-dark">
-        <div class="container">
-            <p class="m-0 text-center text-white">Copyright &copy; Your Website 2021</p>
-        </div>
-    </footer>
+    <!--Footer-->
+    <?php include(ROOT_PATH . "/includes/footer.php"); ?>
+
     <!-- Bootstrap core JS-->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
     <!-- Core theme JS-->
